dd68fbb86100d0d3da08172505e7c564cc5bd3e7
Maria Pilar Guerra Arias <pili@3scale.net>
2016-06-09 21:30:34 +0200

    Add credentials + read webhook_url from notifier arguments

--- monitor-notifier-slack.py
+++ monitor-notifier-slack.py
@@ -2,19 +2,25 @@
 import pika
 import json
 import requests
+import os
 
-SLACK_WEBHOOK_URL = os.environ["SLACK_WEBHOOK_URL"]
 RABBIT_MQ_SERVER = os.environ["RABBIT_MQ_SERVER"]
+RABBIT_MQ_USER = os.environ["RABBIT_MQ_USER"]
+RABBIT_MQ_PWD = os.environ["RABBIT_MQ_PWD"]
+
+credentials = pika.PlainCredentials(RABBIT_MQ_USER, RABBIT_MQ_PWD)
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
-               RABBIT_MQ_SERVER))
+               RABBIT_MQ_SERVER, credentials = credentials))
 channel = connection.channel()
-channel.queue_declare(queue='slack')
+# channel.queue_declare(queue='slack')
 
 def callback(ch, method, properties, body):
     payload = {}
     payload["text"] = body
-    r = requests.post(SLACK_WEBHOOK_URL, data = json.dumps(payload))
+    req = json.loads(body)
+    webhook_url = json.loads(req["monitor"]["notifier"]["arguments"])["webhook_url"]
+    r = requests.post(webhook_url, data = json.dumps(payload))
 
 channel.basic_consume(callback, queue='slack', no_ack=True)
 channel.start_consuming()
